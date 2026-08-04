b5c1d1698fdc5e0863c81e0b4263de8229bdbc77
Ritesh004-kira <riteshvish94@gmail.com>
2026-08-04 23:17:39 +0530

    feat: update solution for best time to buy and sell stock problem

--- dsa_leetcode/best_time_to_buy_and_sell.cpp
+++ dsa_leetcode/best_time_to_buy_and_sell.cpp
@@ -5,9 +5,9 @@ public:
         int max_prof = 0;
         for(int i=1;i<prices.size();i++)
         {
-            max_prof = max(max_prof,prices[i]-min_price);
-            min_price = min(prices[i], min_price);
+            max_prof = max(max_prof,prices[i]-min_price);  // calculating max profit
+            min_price = min(prices[i], min_price);         // updating min price
         }
-        return max_prof;
+        return max_prof; 
     }
 };
