a650b96349c304a168c6a123d774d4c90f6f9e64
Ritesh004-kira <riteshvish94@gmail.com>
2026-08-16 22:28:35 +0530

    feat: improved Contains Duplicate II solution

--- dsa_leetcode/contains_duplicate_II.cpp
+++ dsa_leetcode/contains_duplicate_II.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        unordered_map <int,int>mp = {};
-        int i,j;
+        unordered_map <int,int>mp = {};        // key -> value (index)
+        int i,j; 
         for(int i=0;i<nums.size();i++)
         {
-            if(mp.find(nums[i]) != mp.end())
+            if(mp.find(nums[i]) != mp.end())  // found the element in map
             {
-                if(abs(i-mp[nums[i]])<=k)
+                if(abs(i-mp[nums[i]])<=k)    // check if the absolute difference of indices is <= k
                     return true;
             }
-            mp[nums[i]]=i;
+            mp[nums[i]]=i;                   // update the index of the element
         }
         return false;
     }
