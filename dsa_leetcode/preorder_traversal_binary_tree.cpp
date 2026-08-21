2a16ef5ec0d343c1ba503db84289702b97752149
Ritesh004-kira <riteshvish94@gmail.com>
2026-08-21 21:27:48 +0530

    feat: add binary tree preorder traversal implementation

---
+++ dsa_leetcode/preorder_traversal_binary_tree.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> v;
+    vector<int> preorderTraversal(TreeNode* root) {
+        
+    stack<TreeNode*> s;
+        while(root||!s.empty()){
+            if(root!=NULL){
+                s.push(root);
+                v.push_back(root->val);
+                root=root->left;
+            }
+            else{
+                root=s.top();
+                s.pop();
+                
+                root=root->right;
+            }
+          }
+         return v;
+    }
+};
